fefea0c79fc3b98cf63229a99c37f5cf6b200178
Iliyan <iliyan.motovski@coretrix.com>
2022-07-29 18:46:22 +0300

    fix

--- iyzipay/checkout_form.go
+++ iyzipay/checkout_form.go
@@ -2,7 +2,7 @@ package iyzipay
 
 type CheckoutForm struct{}
 
-func (checkoutForm CheckoutForm) Retrieve(request RetrieveCheckoutFormRequest, options Options) string {
+func (checkoutForm CheckoutForm) Retrieve(request RetrieveCheckoutFormRequest, options Options) *CheckoutFormResponse {
 	response := makeRequest(request, "POST", "/payment/iyzipos/checkoutform/auth/ecom/detail", options)
-	return response
+	return decodeResponse(response, &CheckoutFormResponse{}).(*CheckoutFormResponse)
 }
--- iyzipay/responses.go
+++ iyzipay/responses.go
@@ -47,4 +47,65 @@ type (
 		Currency             string  `json:"currency"`
 		ConnectorName        string  `json:"connectorName"`
 	}
+
+	CheckoutFormResponse struct {
+		Status                       string  `json:"status"`
+		Locale                       string  `json:"locale"`
+		SystemTime                   int64   `json:"systemTime"`
+		Price                        float64 `json:"price"`
+		PaidPrice                    float64 `json:"paidPrice"`
+		Installment                  int     `json:"installment"`
+		PaymentId                    string  `json:"paymentId"`
+		FraudStatus                  int     `json:"fraudStatus"`
+		MerchantCommissionRate       float64 `json:"merchantCommissionRate"`
+		MerchantCommissionRateAmount float64 `json:"merchantCommissionRateAmount"`
+		IyziCommissionRateAmount     float64 `json:"iyziCommissionRateAmount"`
+		IyziCommissionFee            float64 `json:"iyziCommissionFee"`
+		CardType                     string  `json:"cardType"`
+		CardAssociation              string  `json:"cardAssociation"`
+		CardFamily                   string  `json:"cardFamily"`
+		BinNumber                    string  `json:"binNumber"`
+		LastFourDigits               string  `json:"lastFourDigits"`
+		BasketId                     string  `json:"basketId"`
+		Currency                     string  `json:"currency"`
+		ItemTransactions             []struct {
+			ItemId                        string  `json:"itemId"`
+			PaymentTransactionId          string  `json:"paymentTransactionId"`
+			TransactionStatus             int     `json:"transactionStatus"`
+			Price                         float64 `json:"price"`
+			PaidPrice                     float64 `json:"paidPrice"`
+			MerchantCommissionRate        float64 `json:"merchantCommissionRate"`
+			MerchantCommissionRateAmount  float64 `json:"merchantCommissionRateAmount"`
+			IyziCommissionRateAmount      float64 `json:"iyziCommissionRateAmount"`
+			IyziCommissionFee             float64 `json:"iyziCommissionFee"`
+			BlockageRate                  float64 `json:"blockageRate"`
+			BlockageRateAmountMerchant    float64 `json:"blockageRateAmountMerchant"`
+			BlockageRateAmountSubMerchant float64 `json:"blockageRateAmountSubMerchant"`
+			BlockageResolvedDate          string  `json:"blockageResolvedDate"`
+			SubMerchantKey                string  `json:"subMerchantKey"`
+			SubMerchantPrice              float64 `json:"subMerchantPrice"`
+			SubMerchantPayoutRate         float64 `json:"subMerchantPayoutRate"`
+			SubMerchantPayoutAmount       float64 `json:"subMerchantPayoutAmount"`
+			MerchantPayoutAmount          float64 `json:"merchantPayoutAmount"`
+			ConvertedPayout               struct {
+				PaidPrice                     float64 `json:"paidPrice"`
+				IyziCommissionRateAmount      float64 `json:"iyziCommissionRateAmount"`
+				IyziCommissionFee             float64 `json:"iyziCommissionFee"`
+				BlockageRateAmountMerchant    float64 `json:"blockageRateAmountMerchant"`
+				BlockageRateAmountSubMerchant float64 `json:"blockageRateAmountSubMerchant"`
+				SubMerchantPayoutAmount       float64 `json:"subMerchantPayoutAmount"`
+				MerchantPayoutAmount          float64 `json:"merchantPayoutAmount"`
+				IyziConversionRate            float64 `json:"iyziConversionRate"`
+				IyziConversionRateAmount      float64 `json:"iyziConversionRateAmount"`
+				Currency                      string  `json:"currency"`
+			} `json:"convertedPayout"`
+		} `json:"itemTransactions"`
+		AuthCode      string `json:"authCode"`
+		Phase         string `json:"phase"`
+		MdStatus      int    `json:"mdStatus"`
+		HostReference string `json:"hostReference"`
+		Token         string `json:"token"`
+		CallbackUrl   string `json:"callbackUrl"`
+		PaymentStatus string `json:"paymentStatus"`
+	}
 )
